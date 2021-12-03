021e80dc7b8178947e93e5eea26406dba31a4237
Kirill <87267763+KrKpr@users.noreply.github.com>
2021-12-03 09:45:08 +0300

    Update main.c

--- lab11/main.c
+++ lab11/main.c
@@ -4,13 +4,11 @@
 int main() {
     struct data ring;
     float square, perimeter;
-    ring = create_struct(ring);
+    printf("Input larger and smaller radii\n");
+    scanf("%f %f", &ring.largerRadius, &ring.smallerRadius);
     square = SquareFunc(ring);
     perimeter = PerimeterFunc(ring);
     printf("Square is %f\n", square);
     printf("Perimeter is %f\n", perimeter);
     return 0;
 }
-
-//Считываение радиусов сделать в main, обращаться
-//к радиусам также с main, убрать структуру в myheader.c (та что снизу)
