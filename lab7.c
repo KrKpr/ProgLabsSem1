52230dc6d1338c00a3f518cbcd38156cabad5e4b
Kirill <87267763+KrKpr@users.noreply.github.com>
2021-12-03 09:36:32 +0300

    Update lab7.c

--- lab7.c
+++ lab7.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <math.h>
 
 enum Paints {
     Aquarelle = 1,
@@ -9,8 +10,10 @@ enum Paints {
 };
 
 struct Otrezok {
-    int x;
-    int y;
+    int x1;
+    int y1;
+    int x2;
+    int y2;
 };
 
 struct bitf {
@@ -25,48 +28,45 @@ union mp3 {
 };
 
 void main() {
-    //Task 1 (отобразить: водоэмульсионная) //поменять названия переменных
-    enum Paints a, b, c, d, e;
-    a = Aquarelle;
-    b = Oil;
-    c = WaterBased;
-    d = Acrylic;
-    e = Enamel;
-    //printf("a = %d\n", a);
-    //printf("b = %d\n", b);
-    printf("%d\n", c);
-    //printf("d = %d\n", d);
-    //printf("e = %d\n", e);
+    //Task 1 (отобразить: водоэмульсионная)
+    enum Paints display;
+    display = WaterBased;
+    printf("%d\n", display);
 
-    //Task 2 //Переписать структуру (и формулу) открезка
+    //Task 2 The segment length formula is √((X2-X1)² + (Y2-Y1)²).
     printf("\n");
     struct Otrezok l;
-    int dlina;
+    float dlina;
 
-    l.x = -7;
-    l.y = 6;
-    dlina = l.y - l.x;
-    printf("%d\n", dlina);
+    l.x1 = 2;
+    l.y1 = 4;
+    l.x2 = 4;
+    l.y2 = 1;
+    //Используем модуль, ибо pow с отрицательными выдает NaN
+    int ownX = abs(l.x2 - l.x1);
+    int ownY = abs(l.y2 - l.y1);
+    dlina = sqrt(pow(ownX, 2) + pow(ownY, 2));
+    printf("%f\n", dlina);
 
-    //Task 3 //Починить вывод на русском
+    //Task 3
     printf("\n");
     union mp3 t;
     scanf("%x", &t.i);
 
     if (t.b.a == 1) {
-        printf("Воспроизведение вкл\n");
+        printf("Play On\n");
     } else {
-        printf("Воспроизведение выкл\n");
+        printf("Play off\n");
     }
     if (t.b.b == 1) {
-        printf("Пауза вкл\n");
+        printf("Pause on\n");
     } else {
-        printf("Пауза выкл\n");
+        printf("Pause off\n");
     }
     if (t.b.c == 1) {
-        printf("Запись вкл\n");
+        printf("Recording on\n");
     } else {
-        printf("Запись выкл\n");
+        printf("Recording off\n");
     }
 
     printf("%d\n", t.b.a);
